b6939bed167721116453d5b0d60c5f57bb00e4b2
912343041@qq.com <912343041@qq.com>
2024-01-29 12:40:34 +0800

    【CODE】增加notification模块

---
+++ gill-notification-worker/src/main/java/com/gill/notification/worker/GillNotificationWorkerApplication.java
@@ -0,0 +1,18 @@
+package com.gill.notification.worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * GillNotificationWorkerApplication
+ *
+ * @author gill
+ * @version 2024/01/29
+ **/
+@SpringBootApplication
+public class GillNotificationWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GillNotificationWorkerApplication.class, args);
+    }
+}
---
+++ gill-notification/src/main/java/com/gill/notification/boss/GillNotificationApplication.java
@@ -0,0 +1,18 @@
+package com.gill.notification.boss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * GillNotificationApplication
+ *
+ * @author gill
+ * @version 2024/01/29
+ **/
+@SpringBootApplication
+public class GillNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GillNotificationApplication.class, args);
+    }
+}
