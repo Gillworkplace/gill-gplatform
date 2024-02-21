6c4de1c462129fba1d7feef7f2871ceb5d2d55b5
912343041@qq.com <912343041@qq.com>
2024-02-21 21:04:45 +0800

    【CODE】user 模块 cleancode

--- gill-user/src/main/java/com/gill/user/config/MvcConfig.java
+++ gill-user/src/main/java/com/gill/user/config/MvcConfig.java
@@ -1,6 +1,5 @@
 package com.gill.user.config;
 
-import com.gill.user.interceptor.AuthInterceptor;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
--- gill-user/src/main/java/com/gill/user/controller/CaptchaController.java
+++ gill-user/src/main/java/com/gill/user/controller/CaptchaController.java
@@ -2,6 +2,7 @@ package com.gill.user.controller;
 
 import cn.hutool.captcha.AbstractCaptcha;
 import com.gill.user.service.CaptchaService;
+import com.gill.web.annotation.IgnoreAuth;
 import com.gill.web.api.Response;
 import java.io.ByteArrayInputStream;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,7 @@ public class CaptchaController {
      * @param randomCode 随机字符串
      * @return 验证码图片二进制流
      */
+    @IgnoreAuth
     @GetMapping("/captcha")
     public Response<InputStreamResource> getCaptcha(@RequestParam("randomCode") String randomCode) {
         AbstractCaptcha captcha = captchaService.generateCaptcha(randomCode);
